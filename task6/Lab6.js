202420f24061c5dcbe987ee7a39f116a2ddca251
Ales <alexkurch100@gmail.com>
2020-03-23 20:20:24 +0300

    lab 6

---
+++ task6/Lab6.js
@@ -0,0 +1,334 @@
+let posts = [
+    {
+        id: '1',
+        description: 'text for 1',
+        createdAt: new Date('2020-03-17T23:00:00'),
+        author: 'Alex',
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: [
+            'poetry', 'flowers'
+        ],
+        likes: [
+            'Shakespeare'
+        ]
+    },
+    {
+        id: '2',
+        description: 'text for 2',
+        createdAt: new Date('2020-03-17T23:00:00'),
+        author: 'Snow',
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: [
+            'poetry', 'flowers'
+        ],
+        likes: [
+            'Shakespeare'
+        ]
+    },
+    {
+        id: '3',
+        description: 'text for 3',
+        createdAt: new Date('2020-03-17T23:00:00'),
+        author: 'Rose',
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: [
+            'poetry', 'flowers'
+        ],
+        likes: [
+            'Shakespeare'
+        ]
+    }, {
+        id: '4',
+        description: 'text for 4',
+        createdAt: new Date('2020-03-17T23:00:00'),
+        author: 'Kitty',
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: [
+            'poetry', 'flowers'
+        ],
+        likes: [
+            'Shakespeare'
+        ]
+    }, {
+        id: '5',
+        description: 'text for 5',
+        createdAt: new Date('2020-03-17T23:00:00'),
+        author: 'Alex',
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: [
+            'poetry', 'flowers'
+        ],
+        likes: [
+            'Shakespeare'
+        ]
+    }, {
+        id: '6',
+        description: 'text for 6',
+        createdAt: new Date('2020-03-17T23:00:00'),
+        author: 'Snow',
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: [
+            'poetry', 'flowers'
+        ],
+        likes: [
+            'Shakespeare'
+        ]
+    }, {
+        id: '7',
+        description: 'text for 7',
+        createdAt: new Date('2020-03-17T23:00:00'),
+        author: 'Rose',
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: [
+            'poetry', 'flowers'
+        ],
+        likes: [
+            'Shakespeare'
+        ]
+    }, {
+        id: '8',
+        description: 'text for 8',
+        createdAt: new Date('2020-03-17T23:00:00'),
+        author: 'Kitty',
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: [
+            'poetry', 'flowers'
+        ],
+        likes: [
+            'Shakespeare'
+        ]
+    }, {
+        id: '9',
+        description: 'text for 9',
+        createdAt: new Date('2020-03-17T23:00:00'),
+        author: 'Fione',
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: [
+            'poetry', 'flowers'
+        ],
+        likes: [
+            'Shakespeare'
+        ]
+    }, {
+        id: '10',
+        description: 'text for 10',
+        createdAt: new Date('2020-03-17T23:00:00'),
+        author: 'Alex',
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: [
+            'poetry', 'flowers'
+        ],
+        likes: [
+            'Shakespeare'
+        ]
+    }, {
+        id: '11',
+        description: 'text for 11',
+        createdAt: new Date('2020-03-17T23:00:00'),
+        author: 'Snow',
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: [
+            'poetry', 'flowers'
+        ],
+        likes: [
+            'Shakespeare'
+        ]
+    }, {
+        id: '12',
+        description: 'Roses are red violets -- are blue',
+        createdAt: new Date('2020-03-17T22:00:00'),
+        author: 'Rose',
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: [
+            'poetry', 'flowers'
+        ],
+        likes: [
+            'Shakespeare'
+        ]
+    }, {
+        id: '13',
+        description: 'text for 13',
+        createdAt: new Date('2020-03-21T23:00:00'),
+        author: 'Kitty',
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: [
+            'poetry', 'flowers'
+        ],
+        likes: [
+            'Shakespeare'
+        ]
+    }, {
+        id: '14',
+        description: 'text for 14',
+        createdAt: new Date('2020-03-17T23:00:00'),
+        author: 'Fiona',
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: [
+            'poetry', 'flowers'
+        ],
+        likes: [
+            'Shakespeare'
+        ]
+    }, {
+        id: '15',
+        description: 'text for 15',
+        createdAt: new Date('2020-03-17T23:00:00'),
+        author: 'Snow',
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: [
+            'poetry', 'flowers'
+        ],
+        likes: [
+            'Shakespeare'
+        ]
+    },
+    {
+        id: '16',
+        description: 'text for 16',
+        createdAt: new Date('2020-03-17T23:00:00'),
+        author: 'Sand',
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: [
+            'poetry', 'flowers'
+        ],
+        likes: [
+            'Shakespeare'
+        ]
+    }, {
+        id: '17',
+        description: 'text for 17',
+        createdAt: new Date('2020-03-17T23:00:00'),
+        author: 'Sea',
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: [
+            'poetry', 'flowers'
+        ],
+        likes: [
+            'Shakespeare'
+        ]
+    }, {
+        id: '18',
+        description: 'text for 18',
+        createdAt: new Date('2020-03-17T23:00:00'),
+        author: 'Alex',
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: [
+            'poetry', 'flowers'
+        ],
+        likes: [
+            'Shakespeare'
+        ]
+    },
+    {
+        id: '19',
+        description: 'text for 19',
+        createdAt: new Date('2020-03-17T23:00:00'),
+        author: 'Rose',
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: [
+            'poetry', 'flowers'
+        ],
+        likes: [
+            'Shakespeare'
+        ]
+    }, {
+        id: '20',
+        description: 'text for 20',
+        createdAt: new Date('2020-03-17T23:00:00'),
+        author: 'Fione',
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: [
+            'poetry', 'flowers'
+        ],
+        likes: [
+            'Shakespeare'
+        ]
+    },
+];
+let module = (function () {
+    function checkValid(post) {
+        if (typeof post.id !== 'string') {
+            console.log("Problem with id");
+            return false
+        }
+        if (typeof post.description !== 'string' || post.description.length >= 200) {
+            console.log("Problem with description");
+            return false
+        }
+        if (Object.prototype.toString.call(post.createdAt) !== '[object Date]') {
+            console.log("Problem with date");
+            return false
+        }
+        if (typeof post.author !== 'string' || post.author.length <= 0) {
+            console.log("Problem with author");
+            return false
+        }
+        if (typeof post.photoLink !== 'string' && post.photoLink !== undefined) {
+            console.log("Problem with author");
+            return false
+        }
+        if(!Array.isArray(post.hashTags)&& post.hashTags !== undefined){
+            console.log("Problem with tags");
+            return false
+        }
+        if(!Array.isArray(post.likes)&& post.likes !== undefined){
+            console.log("Problem with likers");
+            return false
+        }
+        return true
+    }
+
+    function addPost(post) {
+        if(!module.checkValid(post)|| posts.find(value => value.id === post.id) !== undefined){
+            return false;
+        }
+        posts.push(post);
+        return true
+    }
+    function editPost(id, post) {
+        let postToEdit = module.getPost(id);
+        if(!postToEdit){
+            return false;
+        }
+        Object.keys(post).forEach(field => postToEdit[field] = post[field]);
+        if (!module.checkValid(module.getPost(id)))
+            return false;
+        return true;
+    }
+    function getPost(id) {
+        return posts.find(value => value.id === id)
+    }
+
+    function removePost(id) {
+        posts = posts.filter(value => value.id !== id);
+        return true
+    }
+    function getPosts(skip, top, filterConfig) {
+            if(filterConfig != null) {
+                return posts.filter(p => (filterConfig.author == null || p.author === filterConfig.author) &&
+                    (filterConfig.tags == null || filterConfig.tags.filter(tag => p.hashTags.includes(tag)).length === filterConfig.tags.length))
+                    .slice(skip, skip + top).sort(a => a.createdAt);
+            }
+            return posts.slice(skip, skip + top).sort(a => a.createdAt);
+    }
+    return {
+        getPost: getPost,
+        removePost: removePost,
+        checkValid: checkValid,
+        addPost: addPost,
+        editPost: editPost,
+        getPosts: getPosts
+    }
+}());
+console.log(module.getPosts(0, 10, {author: 'Rose', tags: ['poetry']}));
+console.log(module.removePost('5'));
+console.log(module.editPost('3', {description: 'edited description'}));
+console.log(module.getPost('3'));
+console.log(module.checkValid(module.getPost('3')));
+console.log(module.addPost({id: '1221',
+    description: 'text for 2',
+    createdAt: new Date('2020-03-17T23:00:00'),
+    author: 'Snow',
+    photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'}));
+console.log(module.addPost(module.getPost('7')));
+
